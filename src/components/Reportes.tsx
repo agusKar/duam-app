a21ec3b617968d84ee6cc6343aabdf710258e5ed
Agustin Karkocki <agustin.karkocki@gmail.com>
2023-03-28 17:42:12 -0300

    adding captcha

--- src/components/Reportes.tsx
+++ src/components/Reportes.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Alert, Button, Table } from 'react-bootstrap'
 import { SECRET_TOKEN } from '../config'
 
@@ -18,13 +18,25 @@ import { SECRET_TOKEN } from '../config'
 //     "resultadoTitulo": "Resultado:"
 // }]
 
+interface Captcha {
+  text: string;
+  userText: string;
+  status: Boolean;
+}
+interface Alert {
+  text: string;
+  status: string;
+}
+
 const Reportes = () => {
   const [email, setEmail] = useState(
     localStorage.getItem("email") ? JSON.parse(localStorage.getItem("email") as string) : '')
   const [alert, setAlert] = useState<boolean>(false);
-  const [alertMsg, setAlertMsg] = useState<string>('');
+  const [alertMsg, setAlertMsg] = useState<Alert>({ text: '', status: '' });
   const [reportes, setReportes] = useState([]);
   const [isOnline, setIsOnline] = useState<boolean>(navigator.onLine);
+  const [captcha, setCaptcha] = useState<Captcha>({ text: '', userText: '', status: false });
+  const canvasRef = useRef(null);
 
   useEffect(() => {
     const handleStatusChange = () => {
@@ -65,24 +77,56 @@ const Reportes = () => {
     loadReportes()
   }, [email])
 
+  useEffect(() => {
+      let text = generateCaptcha()
+      setCaptcha({ ...captcha, text })
+  }, [])
+
+  const generateCaptcha = (): string => {
+    let uniquechar: string = "";
+    if(canvasRef.current){
+      const canvas: HTMLCanvasElement = canvasRef.current;
+      const randomchar: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    
+      for (let i = 1; i < 8; i++) {
+        uniquechar += randomchar.charAt(Math.random() * randomchar.length)
+      }
+      
+      let ctx = canvas.getContext("2d");
+      ctx!.clearRect(0, 0, canvas.width, canvas.height);
+
+      ctx!.font = "30px Arial";
+      ctx!.fillStyle = "white";
+      ctx!.fillText(uniquechar, 25, 35);
+
+    }    
+    return uniquechar
+  }
+
   const handleClick = () => {
-    fetch(`https://www.duam.ar/api/items/send?email=${email}`, {
-      headers: { 'Authorization': SECRET_TOKEN }
-    })
-      .then(res => res.json())
-      .then(
-        (result) => {
-          if (!result.status) {
-            throw new Error("Not 200 response", { cause: result.message });
-          } else {
-            setAlertMsg(result.message)
-          }
-        }
-      )
-      .catch(error => {
-        setAlertMsg('')
-        console.log(error)
+    if (captcha.text === captcha.userText) {
+      setCaptcha({ ...captcha, status: true })
+      fetch(`https://www.duam.ar/api/items/send?email=${email}`, {
+        headers: { 'Authorization': SECRET_TOKEN }
       })
+        .then(res => res.json())
+        .then(
+          (result) => {
+            if (!result.status) {
+              throw new Error("Not 200 response", { cause: result.message });
+            } else {
+              setAlertMsg({ text: result.message, status: 'success' })
+              setCaptcha({ ...captcha, userText: '', status: false })
+            }
+          }
+        )
+        .catch(error => {
+          setAlertMsg({ text: error, status: 'danger' })
+          console.log(error)
+        })
+    } else {
+      setAlertMsg({ text: 'Error al ingresar el captcha', status: 'danger' })
+    }
   }
 
   return (
@@ -146,17 +190,20 @@ const Reportes = () => {
               } */}
               </tbody>
             </Table>
-            {isOnline ? (
-              <Button className='mt-4' onClick={() => handleClick()}>Enviar reporte</Button>
-            ) : (
-              <Alert variant='warning' className='mt-4'>No tenés conexión a internet. Volvé a intentarlo nuevamente más tarde.</Alert>
-            )}
-            {
-              alertMsg != '' &&
-              <Alert variant='success' className='mt-4'>El email se envió correctamente.</Alert>
-              
+            {isOnline ?
+                <div className='text-center'>
+                  <canvas ref={canvasRef} id="valicode" width="200" height="50" style={{ "backgroundColor": "#f1701f", "borderRadius": "15px" }}></canvas>
+                  <input type="text" className='form-control bg-white mt-2' placeholder='Ingrese captcha para enviar reporte' value={captcha.userText} onChange={(e) => setCaptcha({ ...captcha, userText: e.target.value })} />
+                  <Button className='text-white mt-4' onClick={() => handleClick()}>Enviar reporte</Button>
+                </div>
+              : (
+                <Alert variant='warning' className='mt-4'>No tenés conexión a internet. Volvé a intentarlo nuevamente más tarde.</Alert>
+              )
             }
-
+            {
+              alertMsg.text !== '' &&
+              <Alert variant={alertMsg.status} className='mt-4'>{alertMsg.text}</Alert>
+            }
           </>
       }
     </>
